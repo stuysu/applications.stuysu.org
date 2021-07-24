28e0e4c50548398c2f8904f4fb74f1f73bdb4a68
Abir Taheer <abir@taheer.me>
2021-07-24 09:09:40 -0400

    imported paginated user result

--- graphql/typeDefs/index.js
+++ graphql/typeDefs/index.js
@@ -2,6 +2,7 @@ import { typeDefs as graphqlScalarDefs } from "graphql-scalars";
 import Application from "./Application";
 import Mutation from "./Mutation";
 import PaginatedApplicationResult from "./PaginatedApplicationResult";
+import PaginatedUserResult from "./PaginatedUserResult";
 import Query from "./Query";
 import User from "./User";
 
@@ -10,6 +11,7 @@ const typeDefs = [
   Application,
   Mutation,
   PaginatedApplicationResult,
+  PaginatedUserResult,
   Query,
   User,
 ];
