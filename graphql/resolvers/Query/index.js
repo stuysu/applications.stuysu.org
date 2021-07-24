c9f276d49b3f0a0a529c68376a9c0772af142a38
Abir Taheer <abir@taheer.me>
2021-07-24 09:12:18 -0400

    imported users to query resolvers

--- graphql/resolvers/Query/index.js
+++ graphql/resolvers/Query/index.js
@@ -2,12 +2,14 @@ import archivedApplications from "./archivedApplications";
 import authenticatedUser from "./authenticatedUser";
 import currentApplications from "./currentApplications";
 import userById from "./userById";
+import users from "./users";
 
 const Query = {
   archivedApplications,
   authenticatedUser,
   currentApplications,
   userById,
+  users,
 };
 
 export default Query;
