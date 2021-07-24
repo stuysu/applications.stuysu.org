65a8dd7f100b8c2260b40cb81896e05deea77c12
Abir Taheer <abir@taheer.me>
2021-07-24 05:48:28 -0400

    imported new typeDefs to apollo

--- graphql/typeDefs/index.js
+++ graphql/typeDefs/index.js
@@ -1,8 +1,17 @@
 import { typeDefs as graphqlScalarDefs } from "graphql-scalars";
+import Application from "./Application";
 import Mutation from "./Mutation";
+import PaginatedApplicationResult from "./PaginatedApplicationResult";
 import Query from "./Query";
 import User from "./User";
 
-const typeDefs = [...graphqlScalarDefs, User, Query, Mutation];
+const typeDefs = [
+  ...graphqlScalarDefs,
+  Application,
+  Mutation,
+  PaginatedApplicationResult,
+  Query,
+  User,
+];
 
 export default typeDefs;
