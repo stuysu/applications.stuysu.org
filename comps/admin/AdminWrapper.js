df0890b99cdb6550b56a659917c1b4008b39daac
Abir Taheer <abir@taheer.me>
2021-08-17 15:42:14 -0400

    added navigation to AuthRequired and centered google button

--- comps/admin/AdminWrapper.js
+++ comps/admin/AdminWrapper.js
@@ -3,6 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 import UserContext from "../auth/UserContext";
+import Navigation from "../nav/Navigation";
 import styles from "./AdminWrapper.module.css";
 import AuthenticationRequired from "./AuthenticationRequired";
 
@@ -27,15 +28,18 @@ export default function AdminWrapper({ children }) {
 
     if (!user.adminPrivileges) {
       return (
-        <div className={styles.container}>
-          <img
-            src={"/access-denied.svg"}
-            alt={"Someone standing next to a phone with an x on it"}
-            className={styles.vector}
-          />
-          <Typography variant={"h3"} color={"error"}>
-            You need to be an admin in order to access this page.
-          </Typography>
+        <div>
+          <Navigation />
+          <div className={styles.container}>
+            <img
+              src={"/access-denied.svg"}
+              alt={"Someone standing next to a phone with an x on it"}
+              className={styles.vector}
+            />
+            <Typography variant={"h3"} color={"error"}>
+              You need to be an admin in order to access this page.
+            </Typography>
+          </div>
         </div>
       );
     }
--- comps/admin/AuthenticationRequired.js
+++ comps/admin/AuthenticationRequired.js
@@ -2,6 +2,7 @@ import Typography from "@material-ui/core/Typography";
 import { useContext } from "react";
 import LoginButton from "../auth/LoginButton";
 import UserContext from "../auth/UserContext";
+import Navigation from "../nav/Navigation";
 import styles from "./AuthenticationRequired.module.css";
 
 export default function AuthenticationRequired({ children }) {
@@ -9,16 +10,21 @@ export default function AuthenticationRequired({ children }) {
 
   if (!user.signedIn) {
     return (
-      <div className={styles.container}>
-        <img
-          src={"/authentication.svg"}
-          alt={"A browser with a lock icon and a person entering a password"}
-          className={styles.vector}
-        />
-        <Typography variant={"h3"} color={"error"} gutterBottom>
-          You need to be logged in to access this page.
-        </Typography>
-        <LoginButton />
+      <div>
+        <Navigation />
+        <div className={styles.container}>
+          <img
+            src={"/authentication.svg"}
+            alt={"A browser with a lock icon and a person entering a password"}
+            className={styles.vector}
+          />
+          <Typography variant={"h3"} color={"error"} gutterBottom>
+            You need to be logged in to access this page.
+          </Typography>
+          <div className={styles.center}>
+            <LoginButton />
+          </div>
+        </div>
       </div>
     );
   }
